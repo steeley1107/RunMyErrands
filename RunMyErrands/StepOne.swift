d3e9782557326904c38bd320802bfa8441c48a0d
Jeff Mew <jeffmew@Jeffs-MacBook-Pro.local>
2016-01-18 14:50:18 -0800

    onboard works

---
+++ RunMyErrands/StepOne.swift
@@ -0,0 +1,35 @@
+//
+//  StepOne.swift
+//  RunMyErrands
+//
+//  Created by Jeff Mew on 2016-01-18.
+//  Copyright © 2016 Jeff Mew. All rights reserved.
+//
+
+import UIKit
+
+class StepOne: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
