910c6731b3ea367650da87c513781b934045ebcc
Steele <Steele@js-MacBook-Pro.local>
2016-08-02 21:55:02 -0400

    using differnt google places methods

--- RunMyErrands/AddErrandViewController.swift
+++ RunMyErrands/AddErrandViewController.swift
@@ -10,90 +10,79 @@ import UIKit
 import GooglePlaces
 
 
-class AddErrandViewController: UIViewController, UISearchDisplayDelegate {
+class AddErrandViewController: UIViewController {
     
-    var searchBar: UISearchBar?
-    var tableDataSource: GMSAutocompleteTableDataSource?
-    var srchDisplayController: UISearchDisplayController?
-
- 
-
+    
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    var resultView: UITextView?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchBar = UISearchBar(frame: CGRectMake(0, 50, 250.0, 44.0))
         
-        tableDataSource = GMSAutocompleteTableDataSource()
-        tableDataSource?.delegate = self
+        resultsViewController = GMSAutocompleteResultsViewController()
+        resultsViewController?.delegate = self
         
-        srchDisplayController = UISearchDisplayController(searchBar: searchBar!, contentsController: self)
-        srchDisplayController?.searchResultsDataSource = tableDataSource
-        srchDisplayController?.searchResultsDelegate = tableDataSource
-        srchDisplayController?.delegate = self
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
         
-        self.view.addSubview(searchBar!)
+        // Put the search bar in the navigation bar.
+        searchController?.searchBar.sizeToFit()
+        self.navigationItem.titleView = searchController?.searchBar
+        
+        // When UISearchController presents the results view, present it in
+        // this view controller, not one further up the chain.
+        self.definesPresentationContext = true
+        
+        // Prevent the navigation bar from being hidden when searching.
+        searchController?.hidesNavigationBarDuringPresentation = false
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    func didUpdateAutocompletePredictionsForTableDataSource(tableDataSource: GMSAutocompleteTableDataSource) {
-        // Turn the network activity indicator off.
-        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-        // Reload table data.
-        srchDisplayController?.searchResultsTableView.reloadData()
-    }
-    
-    func didRequestAutocompletePredictionsForTableDataSource(tableDataSource: GMSAutocompleteTableDataSource) {
-        // Turn the network activity indicator on.
-        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-        // Reload table data.
-        srchDisplayController?.searchResultsTableView.reloadData()
-    }
-    
 }
-
-extension AddErrandViewController: GMSAutocompleteTableDataSourceDelegate {
-    func tableDataSource(tableDataSource: GMSAutocompleteTableDataSource, didAutocompleteWithPlace place: GMSPlace) {
-        srchDisplayController?.active = false
-        // Do something with the selected place.
-        print("Place name: \(place.name)")
-        print("Place address: \(place.formattedAddress)")
-        print("Place attributions: \(place.attributions)")
-        print("Place lat and lon \(place.coordinate)")
-    }
-    
-    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool {
-        tableDataSource?.sourceTextHasChanged(searchString)
-        return false
-    }
-    
-    func tableDataSource(tableDataSource: GMSAutocompleteTableDataSource, didFailAutocompleteWithError error: NSError) {
-        // TODO: Handle the error.
-        print("Error: \(error.description)")
-    }
-    
-    func tableDataSource(tableDataSource: GMSAutocompleteTableDataSource, didSelectPrediction prediction: GMSAutocompletePrediction) -> Bool {
-        return true
-    }
-    
-    
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Handle the user's selection.
+    extension AddErrandViewController: GMSAutocompleteResultsViewControllerDelegate {
+        func resultsController(resultsController: GMSAutocompleteResultsViewController!,
+                               didAutocompleteWithPlace place: GMSPlace!) {
+            searchController?.active = false
+            // Do something with the selected place.
+            print("Place name: ", place.name)
+            print("Place address: ", place.formattedAddress)
+            print("Place attributions: ", place.attributions)
+        }
+        
+        func resultsController(resultsController: GMSAutocompleteResultsViewController!,
+                               didFailAutocompleteWithError error: NSError!){
+            // TODO: handle the error.
+            print("Error: ", error.description)
+        }
+        
+        // Turn the network activity indicator on and off again.
+        func didRequestAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController!) {
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        }
+        
+        func didUpdateAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController!) {
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        }
+        
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+         // Get the new view controller using segue.destinationViewController.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
 }
